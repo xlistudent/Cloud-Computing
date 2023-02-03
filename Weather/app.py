4cb998a20344fd8a0c246dc78439bef5c3c96c14
Xinye L <118423908+xlistudent@users.noreply.github.com>
2023-02-02 16:30:38 -0800

    Add files via upload

---
+++ Weather/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+weathers = {
+    '11111': {'temperature': 72, 'condition': 'Sunny'},
+    '22222': {'temperature': 80, 'condition': 'Sunny'},
+    '33333': {'temperature': 60, 'condition': 'Cloudy'}
+}
+
+class Weather(Resource):
+    def get(self, zipcode):
+
+        if zipcode in weathers:
+            return weathers[zipcode]
+        else:
+            return {'message': 'Zipcode not found'}, 404
+
+api.add_resource(Weather, '/weather/<string:zipcode>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
